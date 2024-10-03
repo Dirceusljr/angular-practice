f6b009f267b925c4662702a317c2efe498c97d2f
dirceusljr <dirceusljr@gmail.com>
2024-10-03 18:46:42 -0300

    feat: :sparkles: feat: setting for Brazilian Currency

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+import { LOCALE_ID } from '@angular/core';
 
-bootstrapApplication(AppComponent).catch((err) => console.error(err));
+registerLocaleData(localePt, 'pt-BR');
+
+bootstrapApplication(AppComponent, {
+    providers: [
+      { provide: LOCALE_ID, useValue: 'pt-BR' }
+    ]
+  }).catch((err) => console.error(err));
